43f92930ba7752b3bb2f430d07fd0e30d4cfaa54
Jelani Woods <jelani@firstdraft.com>
2022-04-07 12:45:00 -0500

    Spring 2022 Updates

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,3 @@
 # Should format and identify different parts of todays date.
 # 
-# "The year is: 2020, the calendar day is: 1, and the month is: 7."
+# "The year is: 2022, the calendar day is: 1, and the month is: 7."
