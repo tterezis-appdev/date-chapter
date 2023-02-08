d91e94dd6c63d9b59f39488c7b9c4bfc6be29a75
Ben Purinton <ben.purinton@gmail.com>
2023-02-08 19:39:20 +0000

    formatted updated

--- formatted.rb
+++ formatted.rb
@@ -17,18 +17,8 @@
 
 # ~~~~~ Specs (make it do these things) ~~~~~
 #
-# math.rb should output '8959' when today is Jul 1, 2020 
-#
-# math.rb is not 'hard-coded' to the current date' 
-#
 # formatted.rb outputs 'The year is: 2020, the calendar day is: 1, and the month is: 7.' when today is July 1, 2020 
 #
 # formatted.rb outputs the formatted dates for any past/future date 
-#
-# formatted.rb should output 'Is today Monday? false', when today is not Monday 
-#
-# monday.rb should output 'Is today Monday? false', when today is not Monday 
-#
-# monday.rb should output 'Is today Monday? false', when today is not Monday 
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ solutions.md
@@ -0,0 +1,19 @@
+# Solutions
+
+## formatted.rb
+
+```ruby
+require "date"
+today = Date.today
+p "The year is: #{today.year}, the calendar day is: #{today.day}, and the month is: #{today.month}."
+```
+
+## math.rb
+
+```ruby
+```
+
+## monday.rb
+
+```ruby
+```
