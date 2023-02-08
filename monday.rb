264f2b3e1e5a8c79dd1c7fc7ecd30b7b9874d941
Ben Purinton <ben.purinton@gmail.com>
2023-02-08 19:32:59 +0000

    remove date_

---
+++ monday.rb
@@ -0,0 +1,29 @@
+# Should output:
+# 
+#   "Is today Monday? true"
+# 
+# if today is Monday, or
+# 
+#   "Is today Monday? false"
+# 
+# if today is not Monday.
+
+
+
+# ~~~~~ Specs (make it do these things) ~~~~~
+#
+# math.rb should output '8959' when today is Jul 1, 2020 
+#
+# math.rb is not 'hard-coded' to the current date' 
+#
+# formatted.rb outputs 'The year is: 2020, the calendar day is: 1, and the month is: 7.' when today is July 1, 2020 
+#
+# formatted.rb outputs the formatted dates for any past/future date 
+#
+# formatted.rb should output 'Is today Monday? false', when today is not Monday 
+#
+# monday.rb should output 'Is today Monday? false', when today is not Monday 
+#
+# monday.rb should output 'Is today Monday? false', when today is not Monday 
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
