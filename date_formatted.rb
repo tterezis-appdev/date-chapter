fb334f59c3bdd77f0e3d9e3f85e4658913806e5e
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-04 00:05:43 +0000

    first commit

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,6 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2022, the calendar day is: 1, and the month is: 7."
+require "date"
+t=Date.today
+p "The year is: #{t.year}, the calendar day is: #{t.day}, and the month is: #{t.month}."
